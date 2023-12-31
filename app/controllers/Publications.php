7dd2c8dc6cc018f93f7d0e3646338a607a5e33de
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-31 15:39:39 +0100

    Update Publications.php

--- app/controllers/Publications.php
+++ app/controllers/Publications.php
@@ -1,7 +1,23 @@
 <?php
 
 Class Publications extends Controller{
+
+    private $publicationModel;
+
+    public function __construct()
+    {
+        $this->publicationModel = $this->model('Publication');
+    }
+
     public function index(){
         echo "hello";
+    }
+
+    public function displayAll()
+    {
+        // echo("zz");
+        $publications= $this->publicationModel->allPublications();
+        echo json_encode($publications);
+
     }
 }
