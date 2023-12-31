33d23f618de0a97c3724b3cadf403c878aaa2d1c
Elgadisalma <elgadi.salma0@gmail.com>
2023-12-31 18:00:07 +0100

    Update Publications.php

--- app/controllers/Publications.php
+++ app/controllers/Publications.php
@@ -18,6 +18,21 @@ Class Publications extends Controller{
         // echo("zz");
         $publications= $this->publicationModel->allPublications();
         echo json_encode($publications);
+        // $this->view('pages/publication' , $publications);
 
+    }
+    public function add(){
+        if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+            $postData = file_get_contents("php://input");
+            $data = json_decode($postData, true);
+            if(!empty($data['msg'])){
+                echo $data['msg'];
+            }else{
+                echo json_encode(['message'=>'invalid request']);
+            }
+
+        }else{
+            echo json_encode(['message'=>'invalid request']);
+        }
     }
 }
